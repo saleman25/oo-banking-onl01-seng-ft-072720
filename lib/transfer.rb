02f8c19caff8e5b0de324b2a1c17d894e58389aa
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 04:57:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
     @receiver.balance += @amount
     @status = "complete"
   else
-    @status = "Transaction rejected. Please check your account balance."
+    @status = "rejected"
 end
 end
  
