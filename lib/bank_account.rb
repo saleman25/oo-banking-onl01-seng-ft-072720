27dcea261a819ce26a3845981543648ef4572a51
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 04:33:52 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
+require 'pry' 
+
 class BankAccount
+attr_reader :name , :balance , :status
+
+def initialize(name)
+@name = name
+@balance = 1000 
+@status = "open"
+end 
+
+def deposit(amount) 
+  @balance += amount
+end 
+
+def display_balance
+  "Your balance is $#{balance}."
+end 
+
+def valid?
+ @broke.balance = 0
+ @closed.status = "closed"
+ @status == "open" && @balance > 0
+ binding.pry
+  end
+
+def close_account
+  @status = "closed"
+end 
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
-  # your code here
+ attr_accessor :amount , :sender , :receiver
+ attr_reader :status , :transfer
+ 
+ def initialize(sender, receiver , amount)
+ @transfer 
+ @amount = amount
+ @sender = sender
+ @receiver = receiver
+ @status = "pending"
+ end
+ 
+ def valid?
+   #sender.valid? && receiver.valid?
+ end 
+ 
+ def execute_transaction
+ end 
+ 
+ def reverse_transfer
+ end 
+ 
+ 
 end
